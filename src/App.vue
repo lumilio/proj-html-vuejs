94243a55c441602be52492dc3614aa01e2ac96f5
lumilio <logistic-division@lumilium.com>
2021-11-25 10:44:27 +0100

    Create vue file , Download bootstrap, Import Image folder.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+
+      <div class="container-fluid bg-dark">xxx</div>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/variables.scss';
+@import "~bootstrap/scss/bootstrap";
+
+
+</style>
